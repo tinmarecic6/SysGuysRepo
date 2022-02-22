22811f7e7eb99f5b5cc209dc0a709b25f056795e
DjStonie <valdonay@yahoo.dk>
2022-02-22 10:29:18 +0100

    Update data_process.py

--- MandatoryHandin1/data_process.py
+++ MandatoryHandin1/data_process.py
@@ -4,14 +4,17 @@ class Partitions:
     # method has to be Independent Output or Concurrent Output
     def __init__(self, numPartitions, threads, method):
         self.numPartitions = numPartitions
-        if (method == "Independent Output")
-        self.arrays = [[] for x in range(numPartitions)]
         with open('data_set.txt', newline='') as f:
-            reader = csv.reader(f)
-            self.data = list(reader)
+                reader = csv.reader(f)
+                self.data = list(reader)
+        if (method == "Concurrent Output"):
+            self.partitionsArray = [[] for x in range(numPartitions)]
 
     def findPartition(self, data):
         return hash(data[0])%self.numPartitions
+    
+    def printPartitions(self):
+        print("Partitions: "+str(self.partitionsArray))
 
-P = Partitions(4)
-print(P.data)
+P = Partitions(10,2,"Concurrent Output")
+P.printPartitions()
