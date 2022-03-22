2d5e172761bc3b8f3e6f75ad1cffb302ecd41a31
tmarecic <tin.marecic@gmail.com>
2022-03-22 14:15:44 +0100

    Modified the code to use ns and to use separate number of partitions in each run.

--- MandatoryHandin1/data_process_v2.py
+++ MandatoryHandin1/data_process_v2.py
@@ -4,7 +4,7 @@ import multiprocessing,time,random,sys
 import os
 
 class Partitions:
-	log_header = f"method|timestamp|throughput|duration(ms)|noTuples|noPartitions|noThreads\n"
+	log_header = f"method|timestamp|throughput|duration(ns)|noTuples|noPartitions|noThreads\n"
 	def __init__(self,numPartitions,threads):
 		self.numPartitions = numPartitions
 		self.threads = threads
@@ -22,7 +22,6 @@ class Partitions:
 	"""
 	Partitioning methods
 	"""
-
 	def independentPartition(self,sharedData,data):
 		#," with data: ",data
 		print("gave job to: ",multiprocessing.current_process().name)
@@ -30,7 +29,7 @@ class Partitions:
 		while sharedData[0] != 0:
 			time.sleep(0.05)
 		if sharedData[1] == 0:
-			sharedData[1] = time.time()
+			sharedData[1] = time.time_ns()
 		localPartitionsArray = [[] for x in range(self.numPartitions)]
 		for i in range(len(data)):
 			localPartitionsArray[self.findPartition(data[i])].append(data[i])
@@ -43,7 +42,7 @@ class Partitions:
 		while sharedData[0] != 0:
 			time.sleep(0.05)
 		if sharedData[1] == 0:
-			sharedData[1] = time.time()
+			sharedData[1] = time.time_ns()
 		for i in range(len(data)):
 			self.partitionsArray[self.findPartition(data[i])].append(data[i])
 		return self.partitionsArray
@@ -53,9 +52,10 @@ class Partitions:
 	def log(self,starttime,duration,noTuples,method,partitions,threads):
 		file = "log_"+starttime.strftime("%Y-%m-%d")+".csv"
 		dttm = starttime.strftime("%Y-%m-%d %H:%M:%S")
-		throughput = noTuples/duration
+		throughput = round(noTuples/(duration/1_000_000_000),2)
+		dur = round(duration,2)
 		with open(file,"a") as f:
-			f.write(f"{method}|{dttm}|{throughput}|{duration}|{noTuples}|{partitions}|{threads}\n")
+			f.write(f"{method}|{dttm}|{throughput}|{dur}|{noTuples}|{partitions}|{threads}\n")
 
 def chunks(l, n):
     """Yield n number of striped chunks from l."""
@@ -65,37 +65,37 @@ def chunks(l, n):
 if __name__ == '__main__':
 	temp = 2
 	threads = int(sys.argv[1])
+	numPartitions = int(sys.argv[2])
 	P = Partitions(temp,threads)
 	filename = "log_"+datetime.now().strftime("%Y-%m-%d")+".csv"
 	if not os.path.isfile(filename):
 		with open(filename,"w") as f:
 			f.write(P.log_header)
-	start = time.time()
+	start = time.time_ns()
 	print("Generating data...")
 	with multiprocessing.Pool(threads) as pool:
 		data = pool.map(P.gen,[x for x in range(0,pow(2,24))])
-		print(f"Generated in {(time.time()-start)} s")
-		for numPartitions in [2,4,6,8,10,12,14,16,18]:
-			print(f"Starting run with {numPartitions} partitions and {threads} threads")
-			P = Partitions(numPartitions,threads)
-			chunksToWork = list(chunks(data,threads))
-			m = multiprocessing.Manager()
-			sharedData = m.list([threads,0])
-			func = partial(P.independentPartition, sharedData)
-			start = time.time()
-			independent_result = pool.map(func, chunksToWork)
-			end_time = (time.time()-start)
-			end_time2 = (time.time()-sharedData[1])
-			print(f"Partitioned independently in {end_time} ms")
-			print(f"Partitioned independently in {end_time2} ms after starting processes")
-			P.log(datetime.now(),end_time2,len(data),"Independent",numPartitions,threads)
-			sharedData[0] = threads
-			sharedData[1] = 0
-			start = time.time()
-			func = partial(P.concurrentPartition, sharedData)
-			concurrent_result = pool.map(func,chunksToWork)
-			end_time = (time.time()-start)
-			end_time2 = (time.time()-sharedData[1])
-			print(f"Partitioned concurrently in {end_time} ms")
-			print(f"Partitioned concurrently in {end_time2} ms after starting processes")
-			P.log(datetime.now(),end_time2,len(data),"Concurrent",numPartitions,threads)
+		print(f"Generated in {(time.time_ns()-start)} ns")
+		print(f"Starting run with {numPartitions} partitions and {threads} threads")
+		P = Partitions(numPartitions,threads)
+		chunksToWork = list(chunks(data,threads))
+		m = multiprocessing.Manager()
+		sharedData = m.list([threads,0])
+		func = partial(P.independentPartition, sharedData)
+		start = time.time_ns()
+		independent_result = pool.map(func, chunksToWork)
+		end_time = (time.time_ns()-start)
+		end_time2 = (time.time_ns()-sharedData[1])
+		print(f"Partitioned independently in {end_time} ns")
+		print(f"Partitioned independently in {end_time2} ns after starting processes")
+		P.log(datetime.now(),end_time2,len(data),"Independent",numPartitions,threads)
+		sharedData[0] = threads
+		sharedData[1] = 0
+		start = time.time_ns()
+		func = partial(P.concurrentPartition, sharedData)
+		concurrent_result = pool.map(func,chunksToWork)
+		end_time = (time.time_ns()-start)
+		end_time2 = (time.time_ns()-sharedData[1])
+		print(f"Partitioned concurrently in {end_time} ns")
+		print(f"Partitioned concurrently in {end_time2} ns after starting processes")
+		P.log(datetime.now(),end_time2,len(data),"Concurrent",numPartitions,threads)
--- MandatoryHandin1/run_test.py
+++ MandatoryHandin1/run_test.py
@@ -1,7 +1,9 @@
 import os
 
 if __name__ == '__main__':
-	threads = 2 
-	maxThreads = 32
-	for t in range(2,maxThreads):
-			os.system(f"python3 data_process_v2.py {t}")
+	threads = [1,2,4,8,16,32]
+	partitions = [1,2,4,6,8,10,12,14,16]
+	for t in threads:
+		for p in partitions:
+			print("Startig on {} partitions and {} threads".format(p,t))
+			os.system(f"python data_process_v2.py {t} {p}")
