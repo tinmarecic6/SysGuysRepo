c4e0d1697ef93a39b8c11b5bb5fbeedd197dfca3
nicolaioestergaard <48184560+nicolaioestergaard@users.noreply.github.com>
2022-03-24 10:59:24 +0100

    update code

--- MandatoryHandin1/run_test.py
+++ MandatoryHandin1/run_test.py
@@ -1,9 +1,9 @@
 import os
 
 if __name__ == '__main__':
-	threads = [1,2,4,8,16,32]
-	partitions = [1,2,4,6,8,10,12,14,16]
+	threads = [1,2,4,8]
+	partitions = [1,2,4,6,8,10,12,14,16,18]
 	for t in threads:
 		for p in partitions:
-			print("Startig on {} partitions and {} threads".format(p,t))
-			os.system(f"python data_process_v2.py {t} {p}")
+			print("Startig on {} threads and {} partitions".format(t,p))
+			os.system("perf stat -o ./logs/23Mar2022Log"+str(t)+"-"+str(p)+".txt -e cycles,instructions,L1-icache-load-misses,L1-dcache-load-misses,LLC-load-misses,cache-misses,stalled-cycles-frontend,stalled-cycles-backend,branch-misses,iTLB-load-misses,dTLB-load-misses ./data_process_v2 {} {}".format(t,p))
